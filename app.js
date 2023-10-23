57182ca3ff3a4acf03c16026ed4bbe086c943863
Akshavya Aggarwal <97899039+Akshavya2509@users.noreply.github.com>
2023-10-23 15:36:30 +0000

    pause, rewind, styling and top-bar

--- app.js
+++ app.js
@@ -1,16 +1,18 @@
 // Scoring configuration (points for each option)
 var optionScores = {
-    option1: 1,
-    option2: 1,
-    option3: 1,
-    option4: 1,
+    option1: 3,
+    option2: 4,
+    option3: 4,
+    option4: 7,
 };
 
 // Timer logic
-var seconds = 600; // 10 minutes
+let x = 5400; // 90 minutes
+var seconds = x;
 var timerInterval;
 var autoSubmit = true; // Flag to indicate whether to auto-submit the form
-
+var timerPaused = false;
+let count = 0;
 function updateTimer() {
     var minutes = Math.floor(seconds / 60);
     var remainingSeconds = seconds % 60;
@@ -34,7 +36,7 @@ function updateTimer() {
 function startTimer() {
     autoSubmit = true; // Reset the auto-submit flag
     clearInterval(timerInterval); // Reset the timer if already running
-    seconds = 600; // Reset the timer to 10 minutes
+    seconds = x; // Set the timer to the specified duration
     updateTimer(); // Display the initial time immediately
     timerInterval = setInterval(updateTimer, 1000);
 }
@@ -43,23 +45,49 @@ function startTimer() {
 function submitForm() {
     // Calculate and display scores
     var selectedOptions = document.querySelectorAll('input[type="checkbox"]:checked');
+    var allOptions = document.querySelectorAll('input[type="checkbox"]');
     var score = 0;
-
+    var totalScore = 0;
+    allOptions.forEach(function (option) {
+        var optionName = option.name;
+        totalScore += optionScores[optionName] || 0;
+    });
     selectedOptions.forEach(function (option) {
         var optionName = option.name;
         score += optionScores[optionName] || 0;
     });
 
     // Display the final score on the page
-    document.getElementById('finalScore').textContent = 'Your final score: ' + score;
+    document.getElementById('finalScore').textContent = 'Your final score: ' + score + '/' + totalScore;
 
     // Reset the timer
     clearInterval(timerInterval);
-    seconds = 600; // Reset the timer to 10 minutes
-    document.getElementById('countdown').textContent = '10:00';
+    seconds = x; // Reset the timer to the specified duration
+    document.getElementById('countdown').textContent = formatTime(x);
 
     // Disable auto-submit when the form is submitted manually
     autoSubmit = false;
     // Additional logic for manual form submission if needed
     // document.getElementById('contestForm').submit();
+}
+
+// Function to format time as MM:SS
+function formatTime(seconds) {
+    var minutes = Math.floor(seconds / 60);
+    var remainingSeconds = seconds % 60;
+    var displayMinutes = minutes < 10 ? '0' + minutes : minutes;
+    var displaySeconds = remainingSeconds < 10 ? '0' + remainingSeconds : remainingSeconds;
+    return displayMinutes + ':' + displaySeconds;
+}
+
+function pauseResumeTimer() {
+    if (!timerPaused) {
+        // If not paused, pause the timer
+        clearInterval(timerInterval);
+        timerPaused = true;
+    } else {
+        // If paused, resume the timer
+        timerPaused = false;
+        timerInterval = setInterval(updateTimer, 1000);
+    }
 }
