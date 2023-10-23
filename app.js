de17f245c3233d3d11c4a7f9f0b42302e0acca49
Akshavya Aggarwal <97899039+Akshavya2509@users.noreply.github.com>
2023-10-23 14:09:56 +0000

    timer working

--- app.js
+++ app.js
@@ -1,22 +1,34 @@
 // Timer logic
-var seconds = 600; // 10 minutes
+x = 10;
+var seconds = x; // 10 minutes
 var timerInterval;
+var autoSubmit = true; // Flag to indicate whether to auto-submit the form
 
 function updateTimer() {
     var minutes = Math.floor(seconds / 60);
     var remainingSeconds = seconds % 60;
-    document.getElementById('countdown').innerHTML = minutes + ':' + (remainingSeconds < 10 ? '0' : '') + remainingSeconds;
+
+    var displayMinutes = minutes < 10 ? '0' + minutes : minutes;
+    var displaySeconds = remainingSeconds < 10 ? '0' + remainingSeconds : remainingSeconds;
+
+    document.getElementById('countdown').textContent = displayMinutes + ':' + displaySeconds;
+
     if (seconds > 0) {
         seconds--;
     } else {
-        // Redirect to submission page when the timer reaches 0
-        document.getElementById('contestForm').submit();
+        clearInterval(timerInterval);
+        if (autoSubmit) {
+            // Auto-submit the form when the timer reaches 0
+            document.getElementById('contestForm').submit();
+        }
     }
 }
 
 function startTimer() {
+    autoSubmit = true; // Reset the auto-submit flag
     clearInterval(timerInterval); // Reset the timer if already running
-    seconds = 600; // Reset the timer to 10 minutes
+    seconds = x; // Reset the timer to 10 minutes
+    updateTimer(); // Display the initial time immediately
     timerInterval = setInterval(updateTimer, 1000);
 }
 
@@ -26,4 +38,9 @@ function submitForm() {
     var selectedOptions = document.querySelectorAll('input[type="checkbox"]:checked');
     var score = selectedOptions.length; // Example: 1 point for each selected option
     alert('Your score: ' + score + ' out of ' + selectedOptions.length);
+
+    // Disable auto-submit when the form is submitted manually
+    autoSubmit = false;
+    // Additional logic for manual form submission if needed
+    // document.getElementById('contestForm').submit();
 }
